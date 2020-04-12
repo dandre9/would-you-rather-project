b148a3182fa38e01b979977808dfbea58d3893ad
Daniel Cancado Caram Andre <dandre9@gmail.com>
2020-04-11 23:10:10 -0300

    feat: Add router to the project

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import { handleInitialData } from "../actions/shared";
 import { AppBar, Toolbar, Button, Container } from "@material-ui/core";
-// import Home from "./Home";
+import Home from "./Home";
 import NewQuestion from "./NewQuestion";
 
 class App extends Component {
@@ -12,20 +13,40 @@ class App extends Component {
 
   render() {
     return (
-      <div className="center">
-        <Container maxWidth="lg">
-          <AppBar position="static">
-            <Toolbar>
-              <Button color="inherit">Home</Button>
-              <Button color="inherit">New Question</Button>
-              <Button color="inherit">Leader Board</Button>
-            </Toolbar>
-          </AppBar>
-          <NewQuestion />
-        </Container>
-      </div>
+      <Router>
+        <div className="center">
+          <Container maxWidth="lg">
+            <AppBar position="static">
+              <Toolbar>
+                <Link to="/">
+                  <Button style={{ color: "white" }}>Home</Button>
+                </Link>
+                <Link to="/add">
+                  <Button style={{ color: "white" }}>New Question</Button>
+                </Link>
+                <Link>
+                  <Button style={{ color: "white" }}>Leader Board</Button>
+                </Link>
+              </Toolbar>
+            </AppBar>
+            {this.props.loading ? null : (
+              <div>
+                <Route path="/" exact component={Home} />
+                <Route path="/add" component={NewQuestion} />
+                {/* <Route path="/tweet/:id" component={TweetPage} /> */}
+              </div>
+            )}
+          </Container>
+        </div>
+      </Router>
     );
   }
 }
 
-export default connect()(App);
+function mapStateToProps({ authedUser }) {
+  return {
+    loading: authedUser === null,
+  };
+}
+
+export default connect(mapStateToProps)(App);
